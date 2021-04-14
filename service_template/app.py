43cf1a9063eeaee98844e8bd82991efdf6728853
ben <bengriffiths1571@gmail.com>
2021-04-14 23:12:57 +0100

    helm fixes

--- service_template/app.py
+++ service_template/app.py
@@ -24,12 +24,14 @@ class PrefixMiddleware(object):
 
 
 app = Flask(__name__)
+
 PREFIX_PATH = os.environ.get("PREFIX_PATH", "")
-DB_URL = os.environ.get("DB_URL", "postgresql://admin:password@db:5432/database")
+DB_HOST = os.environ.get("DB_HOST", "db:5432")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "password")
 
 app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix=PREFIX_PATH)
 
-db_engine = create_engine(DB_URL)
+db_engine = create_engine(f"postgresql://postgres:{DB_PASSWORD}@{DB_HOST}/postgres")
 Base.metadata.bind = db_engine
 
 health_service = HealthService(db_engine)
